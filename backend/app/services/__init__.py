04c4b327c37e40c34ce26f1c178bd3ee7fdbff8b
colehenry <crhenry81@gmail.com>
2026-01-30 15:30:58 -0800

    update readme and import services

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -6,5 +6,15 @@ Separates data access logic from API routing logic.
 """
 
 from .results_service import ResultsService
+from .driver_service import DriverService
+from .constructor_service import ConstructorService
+from .circuit_service import CircuitService
+from .event_service import EventService
 
-__all__ = ["ResultsService"]
+__all__ = [
+    "ResultsService",
+    "DriverService",
+    "ConstructorService",
+    "CircuitService",
+    "EventService",
+]
