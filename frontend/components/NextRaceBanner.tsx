ef85a065c19ff16e81168cf34dea6eab2206e20b
colehenry <crhenry81@gmail.com>
2026-01-30 16:47:05 -0800

    navbar is invisible until used

--- frontend/components/NextRaceBanner.tsx
+++ frontend/components/NextRaceBanner.tsx
@@ -84,7 +84,7 @@ export default function NextRaceBanner() {
             Upcoming Events
           </h2>
           {/* Horizontal Scrollable Events */}
-          <div className="overflow-x-auto overflow-y-hidden -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 scrollbar-thin scrollbar-thumb-purple-500/20 scrollbar-track-transparent hover:scrollbar-thumb-purple-500/40">
+          <div className="overflow-x-auto overflow-y-hidden -mx-4 px-4 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 scrollbar-permanent">
             <div className="flex gap-4 pb-4 min-w-min">
               {events.map((event) => {
                 const daysUntil = getDaysUntilEvent(event.event_date);
