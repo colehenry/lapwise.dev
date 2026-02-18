3c4c62fe252f9b432a1efc2e7a7936ae5f9a66e5
colehenry <crhenry81@gmail.com>
2026-02-17 20:26:58 -0800

    update

--- backend/alembic/versions/3f8a9b2c1d7e_add_jolpica_id_to_drivers_make_driver_.py
+++ backend/alembic/versions/3f8a9b2c1d7e_add_jolpica_id_to_drivers_make_driver_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "3f8a9b2c1d7e"
-down_revision: Union[str, None] = "09410d008af8"
+down_revision: Union[str, None] = "c30bf53f6a6d"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
